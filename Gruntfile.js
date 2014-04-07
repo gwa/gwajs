06e54b9f5468efec5bff8d906d63dd1e59591040
Timothy Groves <groves@greatwhiteark.com>
2014-04-07 17:29:36 +0200

    Add bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,20 +7,17 @@ module.exports = function(grunt) {
 
 		// tasks
 		jscs: grunt.file.readJSON('./grunt/tasks/jscs.json'),
-		uglify: grunt.file.readJSON('./grunt/tasks/uglify.json'),
 		jshint: grunt.file.readJSON('./grunt/tasks/jshint.json')
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-jscs-checker');
 
 	grunt.registerTask(
 		'default',
 		[
 			'jscs',
-			'jshint:src',
-			'uglify'
+			'jshint:src'
 		]
 	);
 
